cb215306bd94a16186dd297f840df539738fed9a
shuo502 <shuo502@163.com>
2018-07-29 13:31:55 +0800

    更新

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -6,15 +6,29 @@ from app.models import Todo
 class TodoTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
-        # print("set up")
+        print("set up")
 
     def tearDown(self):
         todos = Todo.objects.all()
         for todo in todos:
             todo.delete()
-        # print("down")
+        print("down")
 
     def test_index(self):
         rv = self.app.get('/')
-        assert "Todo" in rv.data
-        # print("hello test")
+        assert b'Todo' in rv.data
+        print("hello test")
+
+    def test_todo(self):
+        self.app.post('/add',data=dict(content=b'testtodo'))
+        todo=Todo.objects.get_or_404(content="testtodo")
+        # todo=Todo.objects.get_or_404(content="abc")
+        # todo=Todo.objects.get(content=b"testtodo")
+        print(todo)
+        assert todo is not None
+
+        #coverage report
+        #coverage run -m unittest discover
+        #python -m unittest discove
+
+
